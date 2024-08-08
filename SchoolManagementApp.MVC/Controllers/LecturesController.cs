d02d380921a7f4c3683bbae43dba9b4a77217ce3
farisabujolban <faris.abujolban@gmail.com>
2024-08-08 02:11:52 -0700

    Added Database,  Views, and Controllers

---
+++ SchoolManagementApp.MVC/Controllers/LecturesController.cs
@@ -0,0 +1,162 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SchoolManagementApp.MVC.Data;
+
+namespace SchoolManagementApp.MVC.Controllers
+{
+    public class LecturesController : Controller
+    {
+        private readonly SchoolManagementDbContext _context;
+
+        public LecturesController(SchoolManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Lectures
+        public async Task<IActionResult> Index()
+        {
+              return _context.Lectures != null ? 
+                          View(await _context.Lectures.ToListAsync()) :
+                          Problem("Entity set 'SchoolManagementDbContext.Lectures'  is null.");
+        }
+
+        // GET: Lectures/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Lectures == null)
+            {
+                return NotFound();
+            }
+
+            var lecture = await _context.Lectures
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (lecture == null)
+            {
+                return NotFound();
+            }
+
+            return View(lecture);
+        }
+
+        // GET: Lectures/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Lectures/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName")] Lecture lecture)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(lecture);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(lecture);
+        }
+
+        // GET: Lectures/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Lectures == null)
+            {
+                return NotFound();
+            }
+
+            var lecture = await _context.Lectures.FindAsync(id);
+            if (lecture == null)
+            {
+                return NotFound();
+            }
+            return View(lecture);
+        }
+
+        // POST: Lectures/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName")] Lecture lecture)
+        {
+            if (id != lecture.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(lecture);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LectureExists(lecture.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(lecture);
+        }
+
+        // GET: Lectures/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Lectures == null)
+            {
+                return NotFound();
+            }
+
+            var lecture = await _context.Lectures
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (lecture == null)
+            {
+                return NotFound();
+            }
+
+            return View(lecture);
+        }
+
+        // POST: Lectures/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Lectures == null)
+            {
+                return Problem("Entity set 'SchoolManagementDbContext.Lectures'  is null.");
+            }
+            var lecture = await _context.Lectures.FindAsync(id);
+            if (lecture != null)
+            {
+                _context.Lectures.Remove(lecture);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LectureExists(int id)
+        {
+          return (_context.Lectures?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ SchoolManagementApp.MVC/Data/SchoolManagementDbContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SchoolManagementApp.MVC.Data;
+
+public partial class SchoolManagementDbContext : DbContext
+{
+    public SchoolManagementDbContext(DbContextOptions<SchoolManagementDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Course> Courses { get; set; }
+
+    public virtual DbSet<Lecture> Lectures { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Course>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Courses__3214EC078EB2E07E");
+
+            entity.HasIndex(e => e.Code, "UQ__Courses__A25C5AA720F19ED9").IsUnique();
+
+            entity.Property(e => e.Code).HasMaxLength(50);
+            entity.Property(e => e.Name).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Lecture>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Lectures__3214EC07EFFC504D");
+
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Students__3214EC072A8B126D");
+
+            entity.Property(e => e.DateofBirth).HasColumnType("date");
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
